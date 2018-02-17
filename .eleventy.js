2ed277976eedf1315821c33734c9e239426ffc8b
Zach Leatherman <zachleatherman@gmail.com>
2018-02-17 12:36:26 -0600

    Updates for 0.2.14

--- .eleventy.js
+++ .eleventy.js
@@ -228,6 +228,9 @@ module.exports = function (eleventyConfig) {
     }
   });
 
+  eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("css");
+
   return {
     templateFormats: ["md", "njk", "html", "liquid"],
 
