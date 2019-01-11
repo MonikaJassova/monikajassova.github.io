d8f685d7379ccb7ce78a8d795a407d3c12151168
Zach Leatherman <zachleatherman@gmail.com>
2019-01-10 22:20:14 -0600

    Using deep data merge (post tag is inherited from posts/posts.json) and updates to use 0.7.0.

--- .eleventy.js
+++ .eleventy.js
@@ -155,6 +155,11 @@ module.exports = function (eleventyConfig) {
     return dt.toISO();
   });
 
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toFormat('yyyy-LL-dd');
+  });
+
   // Get the first `n` elements of a collection.
   eleventyConfig.addFilter("head", (array, n) => {
     if (n < 0) {
